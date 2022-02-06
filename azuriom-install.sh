7092fc740eade7863e1525110b251e181c792c62
Divalo33 <97359469+Divalo33@users.noreply.github.com>
2022-02-06 02:51:05 +0100

    Update azuriom-install.sh

--- azuriom-install.sh
+++ azuriom-install.sh
@@ -390,7 +390,6 @@ function aptinstall_phpmyadmin() {
     chown www-data:www-data /usr/share/phpmyadmin/tmp
     chmod 700 /usr/share/phpmyadmin/tmp
     randomBlowfishSecret=$(openssl rand -base64 32)
-    sed -e "s|cfg\['blowfish_secret'\] = ''|cfg['blowfish_secret'] = '$randomBlowfishSecret'|" /usr/share/phpmyadmin/config.sample.inc.php >/usr/share/phpmyadmin/config.inc.php
     ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
 	if [[ "$webserver" =~ (nginx) ]]; then
       apt-get update && apt-get install php8.0{,-bcmath,-mbstring,-common,-xml,-curl,-gd,-zip,-mysql,-fpm} -y
